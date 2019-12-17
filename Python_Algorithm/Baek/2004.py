e8b3c0fdf31015f39e518d7ac752a7363ef02745
HoeYeon <shy9546@naver.com>
2019-12-17 23:42:02 +0900

    Math_12/17

--- Python_Algorithm/Baek/2004.py
+++ Python_Algorithm/Baek/2004.py
@@ -1,10 +1,14 @@
+arr = [5**i for i in range(1,14)]
+arr2 = [2**i for i in range(1,35)]
 n,m = map(int,input().split())
-arr = [0 for i in range(n+1)]
-for i in range(n-m,n+1):
-    arr[i] = arr[i-1]
-    idx = i
-    if i%5 == 0:
-        while i % 5 == 0:
-            i //=5
-            arr[idx] += 1
-print(arr[n])
+result = 0
+for i in arr:
+    result += n//i
+    result -= m//i
+    result -= (n-m)//i
+result2 = 0
+for j in arr2:
+    result2 += n//j
+    result2 -= m//j
+    result2 -= (n-m)//j
+print(min(result2,result))
