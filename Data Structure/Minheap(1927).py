142e6a9328ff2468c0897d6ee54aee518f3b1975
HoeYeon <shy9546@naver.com>
2020-01-12 21:16:13 +0900

    0112

---
+++ Data Structure/Minheap(1927).py
@@ -0,0 +1,44 @@
+import sys
+from math import log
+input = sys.stdin.readline
+
+def upCheck(start):
+    while start//2 > 0:
+        if arr[start] < arr[start//2]:
+            arr[start],arr[start//2] = arr[start//2],arr[start]
+            start //= 2
+        else:
+            break
+def downCheck(idx):
+    start = 1
+    arr[start] = arr[idx]
+    arr[idx] = 2**31
+    while arr[start] != 2**31:
+        tt = start*2 if arr[start*2] < arr[start*2+1] else start*2+1
+        if arr[start] < arr[tt]:
+            break
+        arr[start],arr[tt] = arr[tt],arr[start]
+        start = tt
+    
+    
+def insert(data,idx):
+    arr[idx] = data
+    upCheck(idx)
+
+
+N = int(input())
+length = 2**(int(log(N,2))+1)
+arr = [2**31 for i in range(length)]
+idx = 1
+result = ''
+for i in range(N):
+    tmp = int(input())
+    if tmp == 0:
+        result += str(arr[1]) +'\n' if arr[1] != 2**31 else str(0)+'\n'
+        idx = 1 if idx == 1 else idx-1
+        downCheck(idx)
+    else:
+        insert(tmp,idx)
+        idx += 1
+
+print(result)
---
+++ Python_Algorithm/Baek/11286.py
@@ -0,0 +1,55 @@
+import sys
+from math import log
+input = sys.stdin.readline
+
+def upCheck(start):
+    while start//2 > 0:
+        if abs(arr[start]) < abs(arr[start//2]):
+            arr[start],arr[start//2] = arr[start//2],arr[start]
+            start //= 2
+        elif abs(arr[start]) == abs(arr[start//2]) and arr[start] < arr[start//2]:
+            arr[start],arr[start//2] = arr[start//2],arr[start]
+            start //= 2
+        else:
+            break
+def downCheck(idx):
+    start = 1
+    arr[start] = arr[idx]
+    arr[idx] = 2**31
+    while arr[start] != 2**31:
+        if abs(arr[start*2]) < abs(arr[start*2+1]):
+            tt = start*2
+        elif abs(arr[start*2]) == abs(arr[start*2+1]):
+            tt = start*2 if arr[start*2] < arr[start*2+1] else start*2+1
+        else:
+            tt = start*2+1
+            
+        if abs(arr[start]) <  abs(arr[tt]):
+            break
+        elif abs(arr[start]) == abs(arr[tt]) and arr[start] < arr[tt]:
+            break
+        arr[start],arr[tt] = arr[tt],arr[start]
+        start = tt
+    
+    
+def insert(data,idx):
+    arr[idx] = data
+    upCheck(idx)
+
+
+N = int(input())
+length = 2**(int(log(N,2))+1)
+arr = [2**31 for i in range(131072)]
+idx = 1
+result = ''
+for i in range(N):
+    tmp = int(input())
+    if tmp == 0:
+        result += str(arr[1]) +'\n' if arr[1] != 2**31 else str(0)+'\n'
+        idx = 1 if idx == 1 else idx-1
+        downCheck(idx)
+    else:
+        insert(tmp,idx)
+        idx += 1
+
+print(result)
