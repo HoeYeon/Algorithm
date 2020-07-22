a38252877b64f952bea5179915e3c46afaab7ccf
Hoeyeon <shy9546@naver.com>
2020-07-22 22:28:02 +0900

    0722

---
+++ Java_Algorithm/Algo10951.java
@@ -0,0 +1,16 @@
+package Java_Algorithm;
+
+import java.util.Scanner;
+
+public class Algo10951 {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+
+        while (s.hasNextInt()) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
